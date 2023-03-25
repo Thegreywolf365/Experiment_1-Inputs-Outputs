33be559fd550e818cd9c28f7c74c48c7527f747f
Thegreywolf365 <srujanborkar@gmail.com>
2023-03-25 10:27:44 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+//Experiment 1 To take a user input and print
+
+#include <stdio.h>
+
+int main()
+{   int var1;
+    printf("Enter a number: ");
+    scanf("%d",&var1);
+    printf("%d",var1);
+    return 0;
+}
